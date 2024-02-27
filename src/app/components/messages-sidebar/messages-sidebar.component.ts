e811a3868adeba51146dc7019d9f06b72420a504
Muhammad Imtiaz Hussain <imtiaz0307@gmail.com>
2024-02-27 16:24:39 +0500

    update message sidebar icons

--- src/app/components/messages-sidebar/messages-sidebar.component.ts
+++ src/app/components/messages-sidebar/messages-sidebar.component.ts
@@ -13,79 +13,92 @@ export class MessagesSidebarComponent {
     {
       id: 1,
       label: "Booking",
-      icon: "",
+      icon: "../../../assets/icons/booking.svg",
+      icon_active: "../../../assets/icons/booking.svg",
       active: true
     },
     {
       id: 2,
       label: "Tracking",
-      icon: "",
+      icon: "../../../assets/icons/tracking.svg",
+      icon_active: "../../../assets/icons/tracking_a.svg",
       active: false
     },
     {
       id: 3,
       label: "Arrived",
-      icon: "",
+      icon: "../../../assets/icons/arrived.svg",
+      icon_active: "../../../assets/icons/arrived_a.svg",
       active: false
     },
     {
       id: 4,
       label: "Paylink",
-      icon: "",
+      icon: "../../../assets/icons/paylink.svg",
+      icon_active: "../../../assets/icons/paylink_a.svg",
       active: false
     },
     {
       id: 5,
       label: "PrePay",
-      icon: "",
+      icon: "../../../assets/icons/prepay.svg",
+      icon_active: "../../../assets/icons/prepay_a.svg",
       active: false
     },
     {
       id: 6,
       label: "Review",
-      icon: "",
+      icon: "../../../assets/icons/review.svg",
+      icon_active: "../../../assets/icons/review_a.svg",
       active: false
     },
     {
       id: 7,
       label: "Driver",
-      icon: "",
+      icon: "../../../assets/icons/driver.svg",
+      icon_active: "../../../assets/icons/driver_a.svg",
       active: false
     },
     {
       id: 8,
       label: "Pre Auth",
-      icon: "",
+      icon: "../../../assets/icons/pre_auth.svg",
+      icon_active: "../../../assets/icons/pre_auth_a.svg",
       active: false
     },
     {
       id: 9,
       label: "Custom 1",
-      icon: "",
+      icon: "../../../assets/icons/custom_1.svg",
+      icon_active: "../../../assets/icons/custom_1_a.svg",
       active: false
     },
     {
       id: 10,
       label: "Custom 2",
-      icon: "",
+      icon: "../../../assets/icons/custom_1.svg",
+      icon_active: "../../../assets/icons/custom_1_a.svg",
       active: false
     },
     {
       id: 11,
       label: "ChatBot",
-      icon: "",
+      icon: "../../../assets/icons/chatbot.svg",
+      icon_active: "../../../assets/icons/chatbot_a.svg",
       active: false
     },
     {
       id: 12,
       label: "Missed Calls",
-      icon: "",
+      icon: "../../../assets/icons/missed_call.svg",
+      icon_active: "../../../assets/icons/missed_call_a.svg",
       active: false
     },
     {
       id: 13,
       label: "No Show",
-      icon: "",
+      icon: "../../../assets/icons/noshow.svg",
+      icon_active: "../../../assets/icons/noshow_a.svg",
       active: false
     },
   ]
