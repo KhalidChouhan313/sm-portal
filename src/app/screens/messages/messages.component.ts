2eeab5439f036c4e518441936e2c359b093749cf
MuhammadOwais <141480957+Muhammad-Owais1@users.noreply.github.com>
2025-01-29 20:45:34 +0500

    Fixed textarea.

--- src/app/screens/messages/messages.component.ts
+++ src/app/screens/messages/messages.component.ts
@@ -65,11 +65,12 @@ export class MessagesComponent implements OnInit{
     },
   ]
 
-  toggleEdit = (id: Number) => {
-
-    this.instanceData.forEach(i => i.id === id ? i.isEditable = !i.isEditable : i)
-    console.log(id)
-  }
+  toggleEdit = (id) => {
+    this.instanceData.forEach((item, index) => {
+      item.isEditable = index === id; // Enable only the clicked one
+    });
+  };
+  
 
   onTextAreaChange(instance: any, newValue: string) {
     instance.value = newValue;
