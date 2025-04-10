611b945e000bd4763782083fe4daa5e3eb8b5b68
MuhammadOwais <141480957+Muhammad-Owais1@users.noreply.github.com>
2025-04-10 13:15:39 +0500

    Done.

--- src/app/screens/qr-stats/qr-stats.component.ts
+++ src/app/screens/qr-stats/qr-stats.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { QrcodeService } from 'src/services/qrcode/qrcode.service';
 
 @Component({
   selector: 'app-qr-stats',
   templateUrl: './qr-stats.component.html',
-  styleUrls: ['./qr-stats.component.css']
+  styleUrls: ['./qr-stats.component.css'],
 })
-export class QrStatsComponent {
+export class QrStatsComponent implements OnInit {
+  qrId: string | null = '';
+  stats: any;
+  ipAddress: string | null = '';
 
+  constructor(private route: ActivatedRoute, private QR: QrcodeService) {}
+
+  ngOnInit(): void {
+    // Fetching query parameters from the URL
+    this.route.queryParams.subscribe((params) => {
+      this.qrId = params['qrId']; // qrId is the query parameter in the URL
+      console.log('QR ID:', this.qrId); // Should log '1000'
+
+      this.QR.getQrCodeStats(params['qrId']).subscribe((res) => {
+        console.log('QR Code Stats:', res);
+
+        this.stats = res;
+        this.ipAddress = res.ipAddress;
+      });
+    });
+  }
 }
