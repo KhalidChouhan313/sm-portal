265cfa4f7961368694e709152317091edbd76bfc
Talha Iqbal <talhaiqbal466@gmail.com>
2025-05-25 19:48:59 +0500

    pull

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://app.taximessages.com:88',
   qrApiUrl: 'https://qrcode-qcnb.onrender.com',
-
   // apiUrl: 'http://localhost:3004'
 };
